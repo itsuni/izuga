ea7c6496acbbaaafbd5dba27b44f6bd85ca03a5f
Gian Esteves <31361395+itsuni@users.noreply.github.com>
2018-06-27 22:49:31 -0400

    windows rework

---
+++ README.md
@@ -0,0 +1,11 @@
+<img align="center" src='https://raw.githubusercontent.com/itsuni/izuga/master/izugatext.png'>
+
+# izuga
+An open source manga reader
+
+Currently under beginning of development.  Don't expect much!
+
+# Future Features
+* MAL and maybe possible other anime list connection
+* Simple and fast manga reader
+* Manga database browser
---
+++ src/izuga/Izuga.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package izuga;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Gian
+ */
+public class Izuga extends Application {
+    
+    private double xOffset = 0;
+    private double yOffset = 0;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
+        stage.initStyle(StageStyle.UNDECORATED);
+        
+        //Move Undecorated Window
+        
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = stage.getX() - event.getScreenX();
+                yOffset = stage.getY() - event.getScreenY();
+            }
+        });
+        
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() + xOffset);
+                stage.setY(event.getScreenY() + yOffset);
+            }
+        });
+        
+        //End of move
+        
+        Scene scene = new Scene(root);
+        Image icon = new Image(getClass().getResourceAsStream("/images/izuga.png"));
+        stage.getIcons().add(icon);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}

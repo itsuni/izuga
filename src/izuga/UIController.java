8a5250c75f6015ed637bd1c768318da06562c33a
Gian Esteves <31361395+itsuni@users.noreply.github.com>
2018-06-28 14:47:19 -0400

    Button Update
    More functionality soon

--- src/izuga/UIController.java
+++ src/izuga/UIController.java
@@ -5,19 +5,38 @@
  */
 package izuga;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
  * @author Gian
  */
 public class UIController implements Initializable {
+    
+    //Name Panels
+    @FXML
+    private AnchorPane pn_popup;
+    
+    //Name Buttons
+    @FXML
+    private JFXButton btn_popupx, btn_start;
+    
+    //Popup Close on Button X
+    @FXML
+    private void handleButtonAction(MouseEvent event) {
+        if(event.getTarget() == btn_popupx || event.getTarget() == btn_start) {
+            pn_popup.setVisible(false);
+        }
+    }
 
-    //Close on click on x
+    //Close application on click on x
     @FXML
     private void handleClose(MouseEvent event) {
         System.exit(0);

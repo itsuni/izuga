d2b5f47cb35fc04b8efcec061fdef17eeb795a6c
Gian Esteves <31361395+itsuni@users.noreply.github.com>
2018-06-28 19:18:01 -0400

    functional menu

--- src/izuga/UIController.java
+++ src/izuga/UIController.java
@@ -11,40 +11,62 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 
 /**
  *
  * @author Gian
  */
 public class UIController implements Initializable {
-    
+
     //Name Panels
     @FXML
     private AnchorPane pn_popup;
-    
+
+    @FXML
+    private Pane pn_library, pn_history, pn_browse, pn_down, pn_settings;
+
     //Name Buttons
     @FXML
-    private JFXButton btn_popupx, btn_start;
-    
-    //Popup Close on Button X
-    @FXML
-    private void handleButtonAction(MouseEvent event) {
-        if(event.getTarget() == btn_popupx || event.getTarget() == btn_start) {
-            pn_popup.setVisible(false);
-        }
-    }
+    private JFXButton btn_popupx, btn_start, btn_library, btn_history, btn_browse, btn_download, btn_settings;
 
     //Close application on click on x
     @FXML
     private void handleClose(MouseEvent event) {
         System.exit(0);
     }
-    
+
+    //Popup Close on Button X
+    @FXML
+    private void handleButtonAction(MouseEvent event) {
+        if (event.getTarget() == btn_popupx || event.getTarget() == btn_start) {
+            pn_popup.setVisible(false);
+        }
+    }
+
+    @FXML
+    private void newButtonClick(ActionEvent event) {
+        if (event.getSource() == btn_library) {
+            pn_library.toFront();
+        } else if (event.getSource() == btn_history) {
+            pn_history.toFront();
+        } else if (event.getSource() == btn_browse) {
+            pn_browse.toFront();
+        } else if (event.getSource() == btn_download) {
+            pn_down.toFront();
+        } else if (event.getSource() == btn_settings) {
+            pn_settings.toFront();
+        }
+    }
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb
+    ) {
         // TODO
-    }    
-    
+    }
+
 }
